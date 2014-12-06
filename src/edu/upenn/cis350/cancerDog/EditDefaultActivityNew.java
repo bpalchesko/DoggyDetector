1311753f85a98a8577d69388d145dbd701df6866
Brad Palchesko <bradpalchesko@gmail.com>
2014-12-06 12:46:56 -0500

    Edit dog/personnel lists and update in shared prefs

--- src/edu/upenn/cis350/cancerDog/EditDefaultActivityNew.java
+++ src/edu/upenn/cis350/cancerDog/EditDefaultActivityNew.java
@@ -27,41 +27,55 @@ public class EditDefaultActivityNew extends Activity implements NumberPicker.OnV
 	NumberPicker settingsBenignNumberPicker;
 	NumberPicker settingsMalignantNumberPicker;
 
-	static ArrayList<String> dogs = new ArrayList<String>();
-	static ArrayList<String> handlers = new ArrayList<String>();
-
+	private ArrayList<String> dogs = new ArrayList<String>();
+	private ArrayList<String> personnel = new ArrayList<String>();
+	private String currentDog;
+	private String currentHandler;
+	private String currentTester;
+	private String currentRecorder;
+	
+	private static final String PERSONNEL_FILE = "edu.upenn.cis350.cancerDog.handlers";
+	private static final String DOG_FILE = "edu.upenn.cis350.cancerDog.dogs";
+	private SharedPreferences personnelSettings;
+	private SharedPreferences dogSettings;
+	private SharedPreferences.Editor personnelEditor; 
+	private SharedPreferences.Editor dogEditor; 
+	
 	private ArrayAdapter<String> dogAdapter;
 	private ArrayAdapter<CharSequence> handlerAdapter;
-
-	private ArrayList<Boolean> set = new ArrayList<Boolean>();
+	private ArrayAdapter<CharSequence> testerAdapter;
+	private ArrayAdapter<CharSequence> recorderAdapter;
+	
 	private Spinner dogSpinner;
 	private Spinner handlerSpinner;
+	private Spinner testerSpinner;
+	private Spinner recorderSpinner;
+	
+	private EditText tempText;
+	private EditText humidityText;
 	
 	/** Called when the activity is first created. */
 	@Override
-	public void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {	
 	    super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_editdefaults_new);
 		Log.e("Loading Activity", "EditDefaultActivityNew");
-		Intent data = (Intent) getIntent();
-		dogs = getGroup(this, "dogs");
-		handlers = getGroup(this, "handlers");
-
-		for (int i = 0; i < 4; i++) {
-			set.add(false);
-		}
+		Intent data = (Intent) getIntent();	
+		dogSettings = getSharedPreferences(DOG_FILE, Context.MODE_PRIVATE);	
+		personnelSettings = getSharedPreferences(PERSONNEL_FILE, Context.MODE_PRIVATE);
+		dogEditor = dogSettings.edit(); //TODO move
+		personnelEditor = personnelSettings.edit(); //TODO move
+		initializeSettings();
+		getSavedSettings();		
 		
 		if (data.hasExtra("Control") && data.hasExtra("Benign") && data.hasExtra("Malignant")) {
-		      bw = new BloodWheel();
-		      bw.Control=data.getExtras().getInt("Control");
-		      bw.Benign=data.getExtras().getInt("Benign");
-		      bw.Malignant=data.getExtras().getInt("Malignant");
-		      Log.e("data", Integer.valueOf(bw.Malignant).toString() + " " + Integer.valueOf(bw.Benign).toString() + " ");
-		}
-		else
-		{
 			bw = new BloodWheel();
-			Log.e("data", "data not received");
+			bw.Control = data.getExtras().getInt("Control");
+			bw.Benign = data.getExtras().getInt("Benign");
+			bw.Malignant = data.getExtras().getInt("Malignant");
+		} else {
+			Log.e("Loading Activity", "bw data not passed to activity");
+			bw = new BloodWheel();
 		}
 		      
 		settingsControlNumberPicker = (NumberPicker) findViewById(R.id.settingsControlNumberPicker);
@@ -69,95 +83,104 @@ public class EditDefaultActivityNew extends Activity implements NumberPicker.OnV
 		settingsControlNumberPicker.setMaxValue(12);
 		settingsControlNumberPicker.setValue(bw.Control);
 		settingsControlNumberPicker.setOnValueChangedListener(this);
-		// -16777216 is the number for black
-		setNumberPickerTextColor(settingsControlNumberPicker, -16777216);
-		
+		setNumberPickerTextColor(settingsControlNumberPicker, -16777216); //number for black		
 		settingsBenignNumberPicker = (NumberPicker) findViewById(R.id.settingsBenignNumberPicker);
 		settingsBenignNumberPicker.setMinValue(1);
 		settingsBenignNumberPicker.setMaxValue(12);
 		settingsBenignNumberPicker.setValue(bw.Benign);
 		settingsBenignNumberPicker.setOnValueChangedListener(this);
-		// -16777216 is the number for black
-		setNumberPickerTextColor(settingsBenignNumberPicker, -16777216);
-		
+		setNumberPickerTextColor(settingsBenignNumberPicker, -16777216); //number for black		
 		settingsMalignantNumberPicker = (NumberPicker) findViewById(R.id.settingsMalignantNumberPicker);
 		settingsMalignantNumberPicker.setMinValue(1);
 		settingsMalignantNumberPicker.setMaxValue(12);
 		settingsMalignantNumberPicker.setValue(bw.Malignant);
 		settingsMalignantNumberPicker.setOnValueChangedListener(this);
-		// -16777216 is the number for black
-		setNumberPickerTextColor(settingsMalignantNumberPicker, -16777216);
+		setNumberPickerTextColor(settingsMalignantNumberPicker, -16777216); //number for black
 		
 		dogAdapter = new ArrayAdapter<String>(this, R.layout.spinner_item);
 		dogAdapter.setDropDownViewResource(R.layout.dropdown_spinner_item);
-		dogAdapter.add("Click to edit");
-		for (String s : dogs) {
-			dogAdapter.add(s);
-		}
-		//dogAdapter.add("Add +"); TODO add back
+		handlerAdapter = new ArrayAdapter<CharSequence>(this, R.layout.spinner_item);
+		handlerAdapter.setDropDownViewResource(R.layout.dropdown_spinner_item);
+		testerAdapter = new ArrayAdapter<CharSequence>(this, R.layout.spinner_item);
+		testerAdapter.setDropDownViewResource(R.layout.dropdown_spinner_item);	
+		recorderAdapter = new ArrayAdapter<CharSequence>(this, R.layout.spinner_item);
+		recorderAdapter.setDropDownViewResource(R.layout.dropdown_spinner_item);	
+		refreshAdapters();
+		
 		dogSpinner = (Spinner) findViewById(R.id.dogSpinner1);
 		dogSpinner.setAdapter(dogAdapter);
+		dogSpinner.setSelection(findCurrentValue("dog"));
 		dogSpinner
 				.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
 					public void onItemSelected(AdapterView<?> parentView,
 							View selectedItemView, int position, long id) {	
-						selected(position, 1, dogs);
 						dogSpinner.setSelection(position);
 						String name = dogSpinner.getSelectedItem().toString();
-						SharedPreferences preferences = getSharedPreferences(
-								"edu.upenn.cis350.cancerDog.dogs", Context.MODE_PRIVATE);
-						SharedPreferences.Editor editor = preferences.edit();
-						editor.clear();
-						editor.putString("current", name);
-						editor.commit();
-						
+						if("Edit List".equals(name)) createEditDialog("dog").show();
+						else currentDog = name;
 					}
 
 					@Override
-					public void onNothingSelected(AdapterView<?> parentView) {
-						// your code here
-					}
+					public void onNothingSelected(AdapterView<?> parentView) {}
 				});
-
-		// for the edit defaults handlers section
-		handlerAdapter = new ArrayAdapter<CharSequence>(this, R.layout.spinner_item);
-		handlerAdapter.setDropDownViewResource(R.layout.dropdown_spinner_item);
-		handlerAdapter.add("Click to edit");
-		for (String s : handlers) {
-			handlerAdapter.add(s);
-		}
-		//handlerAdapter.add("Add +"); TODO add back
 
 		handlerSpinner = (Spinner) findViewById(R.id.handlerSpinner1);
 		handlerSpinner.setAdapter(handlerAdapter);
-		handlerSpinner
-				.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+		handlerSpinner.setSelection(findCurrentValue("handler"));
+		handlerSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
 					public void onItemSelected(AdapterView<?> parentView,
 							View selectedItemView, int position, long id) {
-						selected(position, 2, handlers);
 						handlerSpinner.setSelection(position);
 						String name = handlerSpinner.getSelectedItem().toString();
-						SharedPreferences preferences = getSharedPreferences(
-								"edu.upenn.cis350.cancerDog.handlers", Context.MODE_PRIVATE);
-						SharedPreferences.Editor editor = preferences.edit();
-						editor.clear();
-						editor.putString("current", name);
-						editor.commit();
-						
+						if ("Edit List".equals(name)) createEditDialog("personnel").show();
+						else currentHandler = name;		
 					}
 
 					@Override
-					public void onNothingSelected(AdapterView<?> parentView) {
-						// your code here
-					}
+					public void onNothingSelected(AdapterView<?> parentView) {}
 				});
+		
+		testerSpinner = (Spinner) findViewById(R.id.testerSpinner);
+		testerSpinner.setAdapter(testerAdapter);
+		testerSpinner.setSelection(findCurrentValue("tester"));
+		testerSpinner
+				.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+					public void onItemSelected(AdapterView<?> parentView,
+							View selectedItemView, int position, long id) {	
+						testerSpinner.setSelection(position);
+						String name = testerSpinner.getSelectedItem().toString();
+						if ("Edit List".equals(name)) createEditDialog("personnel").show();
+						else currentTester = name;					
+					}
+
+					@Override
+					public void onNothingSelected(AdapterView<?> parentView) {}
+				});
+
+		recorderSpinner = (Spinner) findViewById(R.id.recorderSpinner);
+		recorderSpinner.setAdapter(recorderAdapter);
+		recorderSpinner.setSelection(findCurrentValue("recorder"));
+		recorderSpinner
+				.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+					public void onItemSelected(AdapterView<?> parentView,
+							View selectedItemView, int position, long id) {	
+						recorderSpinner.setSelection(position);
+						String name = recorderSpinner.getSelectedItem().toString();
+						if ("Edit List".equals(name)) createEditDialog("personnel").show();
+						else currentRecorder = name;						
+					}
+
+					@Override
+					public void onNothingSelected(AdapterView<?> parentView) {}
+				});
+		
+		tempText = (EditText) findViewById(R.id.tempText);
+		humidityText = (EditText) findViewById(R.id.humidityText);
+		
 	}
 
 	@Override
-	public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
-		// TODO Auto-generated method stub
-
-	}
+	public void onValueChange(NumberPicker picker, int oldVal, int newVal) {}
 
 	/**
 	 * Credit to StackOverflow
@@ -193,135 +216,137 @@ public class EditDefaultActivityNew extends Activity implements NumberPicker.OnV
 		return false;
 	}
 
-	// what is called when an option in the spinner is clicked on
-	public void selected(int position, int which, ArrayList<String> list) {
-		if (!set.get(0)) {
-			set.remove(which);
-			set.add(which, true);
-		} else {
-//			if (position != 0) { //TODO add back with list.size() - 1
-//				if (position <= list.size()) {
-//					deletePrompt(list, position - 1, 0);
-//				} else {
-//					addPrompt(list);
-//					
-//				}
-//			}
-		}
-	}
-
-	// when you click on an already created option, it will ask if you want to
-	// delete it or not (yes, or no). If you answer yes, this method will be
-	// called.
-	public void deletePrompt(final ArrayList<String> list, final int position,
-			final int drop) {
-		AlertDialog.Builder temp = new AlertDialog.Builder(
-				EditDefaultActivityNew.this);
-		temp.setMessage("Are you sure you want to delete " + list.get(position));
-		temp.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-			public void onClick(DialogInterface dialog, int id) {
-				list.remove(position);
-				refreshAdapters();
-				saveGroup();
-			}
-		});
-		temp.setNegativeButton("No", new DialogInterface.OnClickListener() {
-			public void onClick(DialogInterface dialog, int id) {
-				// User cancelled the dialog
-			}
-		});
-		AlertDialog tempDialog = temp.create();
-		tempDialog.show();
-	}
-
-	// if you want to add a default value and click add, this method is called
-	public void addPrompt(final ArrayList<String> list) {
-		final EditText input = new EditText(EditDefaultActivityNew.this);
-
-		AlertDialog.Builder temp = new AlertDialog.Builder(
-				EditDefaultActivityNew.this);
-		temp.setTitle("Add");
-
-		temp.setView(input);
-
-		temp.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-			public void onClick(DialogInterface dialog, int whichButton) {
-				String text = input.getText().toString();
-				list.add(text);
-				refreshAdapters();
-				saveGroup();
-			}
-		});
-		temp.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-			public void onClick(DialogInterface dialog, int whichButton) {
-				// Canceled.
-			}
-		});
-
-		AlertDialog tempDialog = temp.create();
-
-		tempDialog.setView(input);
-
-		tempDialog.show();
-	}
-
 	// this is shown as a default on the spinner for the edit default screen
 	public void refreshAdapters() {
 		dogAdapter.clear();
-		dogAdapter.add("Click to edit");
+		handlerAdapter.clear();
+		testerAdapter.clear();
+		recorderAdapter.clear();
 		for (String s : dogs) {
 			dogAdapter.add(s);
-		}
-		//dogAdapter.add("Add +"); TODO add back
-		
-		handlerAdapter.clear();
-		handlerAdapter.add("Click to edit");
-		for (String s : handlers) {
+		}	
+		for (String s : personnel) {
 			handlerAdapter.add(s);
+			testerAdapter.add(s);
+			recorderAdapter.add(s);
 		}
-		//handlerAdapter.add("Add +"); TODO add back
+		dogAdapter.add("Edit List");
+		handlerAdapter.add("Edit List");
+		testerAdapter.add("Edit List");
+		recorderAdapter.add("Edit List");
 	}
 
+	//Set spinners to current selection, or first element if selection was deleted
+	public int findCurrentValue(String value) {
+		if("dog".equals(value)) {
+			return Math.max(dogs.indexOf(currentDog), 0);
+		} else if("handler".equals(value)) {
+			return Math.max(personnel.indexOf(currentHandler), 0);
+		} else if("tester".equals(value)) {
+			return Math.max(personnel.indexOf(currentTester), 0);			
+		} else if("recorder".equals(value)) {
+			return Math.max(personnel.indexOf(currentRecorder), 0);		
+		}
+		return 0;
+	}
+		
+	public AlertDialog.Builder createEditDialog(final String category) {
+		AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+		final EditText input = new EditText(this);
+		if("dog".equals(category)) {
+			dialog.setTitle("Edit Dog List");
+			input.setText(nameStringFromList(dogs));
+		} else {
+			dialog.setTitle("Edit Personnel List");
+			input.setText(nameStringFromList(personnel));
+		}
+		dialog.setMessage("Please enter names separated by commas \n" +
+				"Example: Name1, Name2, Name3, Name4");
+		dialog.setView(input);
+
+		dialog.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int whichButton) {
+				String group = input.getText().toString();
+				if ("dog".equals(category)) {
+					dogs = listFromNameString(group);	
+				} else {
+					personnel = listFromNameString(group);
+				}
+				refreshAdapters();			
+				dogSpinner.setSelection(findCurrentValue("dog"));
+				handlerSpinner.setSelection(findCurrentValue("handler"));
+				testerSpinner.setSelection(findCurrentValue("tester"));
+				recorderSpinner.setSelection(findCurrentValue("recorder"));
+			}
+		});
+	
+		dialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+			  public void onClick(DialogInterface dialog, int whichButton) {
+			    // Canceled.
+			  }
+		});
+		return dialog;
+	}
+	
+	
 	// when the default spinner is clicked, this is called to get the
 	// information in the spinner
-	public static ArrayList<String> getGroup(Context c, String group) {
-		ArrayList<String> temp = new ArrayList<String>();
-		if (group == "dogs") {
-		String[] dogArray = c.getResources().getStringArray(R.array.dogs);
-		for (String s : dogArray) {
-			temp.add(s);
+	public ArrayList<String> getGroup(String group) {
+		String names = "";
+		if ("dogs".equals(group)) {
+			SharedPreferences settings = getSharedPreferences(
+					DOG_FILE, Context.MODE_PRIVATE);
+			names = settings.getString("dogs", "EMPTY");
+		} else if ("handlers".equals(group)) {
+			SharedPreferences settings = getSharedPreferences(
+					PERSONNEL_FILE, Context.MODE_PRIVATE);
+			names = settings.getString("handlers", "EMPTY");
 		}
-		} else if (group == "handlers") {
-			String[] handlerArray = c.getResources().getStringArray(R.array.handlers);
-			for (String s : handlerArray) {
-				temp.add(s);
-			}
+		if("EMPTY".equals(names)) {
+			return new ArrayList<String>();
+		}
+		return listFromNameString(names);
+	}
+	
+	public void getSavedSettings(){
+		dogs = getGroup("dogs");
+		personnel= getGroup("handlers");
+		currentDog = dogSettings.getString("current", "");
+		currentHandler = personnelSettings.getString("current_handler", "");
+		currentTester = personnelSettings.getString("current_tester", "");
+		currentRecorder = personnelSettings.getString("current_recorder", "");
+	}
+	
+	public void saveCurrentSettings() {
+		dogEditor.putString("current", currentDog);
+		dogEditor.putString("dogs", nameStringFromList(dogs));
+		dogEditor.commit();	
+		personnelEditor.putString("current_handler", currentHandler);
+		personnelEditor.putString("current_tester", currentTester);
+		personnelEditor.putString("current_recorder", currentRecorder);
+		personnelEditor.putString("handlers", nameStringFromList(personnel));
+		personnelEditor.commit();
+		
+	}
+	
+	public static ArrayList<String> listFromNameString(String names) {
+		ArrayList<String> temp = new ArrayList<String>();
+		names = names.trim();
+		String[] namesArray = names.split(",");
+		for(String s: namesArray) {
+			s = s.trim();
+			temp.add(s);
 		}
 		return temp;
 	}
-
-	// when you delete a default option, this method is called to ensure that
-	// you save the spinner without the default method that was deleted
-	public void saveGroup() {
-		SharedPreferences preferences = this.getSharedPreferences(
-				"edu.upenn.cis350.cancerDog.dogs", Context.MODE_PRIVATE);
-		SharedPreferences.Editor editor = preferences.edit();
-
-		editor.clear();
-		for (int i = 0; i < dogs.size(); i++) {
-			editor.putString(String.valueOf(i), dogs.get(i));
+	
+	public static String nameStringFromList(ArrayList<String> names) {
+		String temp = "";
+		for(int i = 0; i < names.size() - 1; i++) {
+			temp = temp + names.get(i) + ", ";
 		}
-		editor.commit();
-
-		preferences = this.getSharedPreferences(
-				"edu.upenn.cis350.cancerDog.handlers", Context.MODE_PRIVATE);
-		editor = preferences.edit();
-
-		editor.clear();
-		for (int i = 0; i < handlers.size(); i++) {
-			editor.putString(String.valueOf(i), handlers.get(i));
-		}
-		editor.commit();
+		temp += names.get(names.size() - 1);
+		return temp;
 	}
 	
 	// when the back button is clicked, this method is called
@@ -331,32 +356,27 @@ public class EditDefaultActivityNew extends Activity implements NumberPicker.OnV
 	}
 
 	@Override
-	public void onActivityResult(int requestCode, int resultCode, Intent data) {//If back on phone pressed, retrieve data for intent
-		super.onActivityResult(requestCode, resultCode, data);
-	    if (data==null)
-	    {
-	    	Log.e("Loading Activity", "nulls datas...");
-	    }
-	    else if (data.hasExtra("Benign")) {
-	    	bw.Benign = data.getExtras().getInt("Benign");
-	    	bw.Control = data.getExtras().getInt("Control");
-	    	bw.Malignant = data.getExtras().getInt("Malignant");
-	    	settingsBenignNumberPicker.setValue(bw.Benign);
-	    	settingsControlNumberPicker.setValue(bw.Control);
-	    	settingsMalignantNumberPicker.setValue(bw.Malignant);
-	  	}
-	}
-	
-	@Override
-	public void finish(){//If back on phone pressed, store data for next intent
+	public void finish(){
 		Intent i = new Intent();
 		bw.Benign=settingsBenignNumberPicker.getValue();
 		bw.Control=settingsControlNumberPicker.getValue();
 		bw.Malignant=settingsMalignantNumberPicker.getValue();
 		i.putExtra("Benign", bw.Benign);
 		i.putExtra("Control", bw.Control);  
-		i.putExtra("Malignant", bw.Malignant); 
+		i.putExtra("Malignant", bw.Malignant);
+		saveCurrentSettings();
 		setResult(Activity.RESULT_OK, i);
 		super.finish();
+	}
+	
+	private void initializeSettings() {
+		String handlers = personnelSettings.getString("handlers", "EMPTY");
+		if("EMPTY".equals(handlers)) {
+			personnelEditor.putString("handlers", "Annemarie D, Jonathan B, Ken V, Sun, Lorenzo R");
+		}
+		String dogs = dogSettings.getString("dogs", "EMPTY");
+		if("EMPTY".equals(dogs)) {
+			dogEditor.putString("dogs", "Tsunami, Ffoster, McBaine, Ohlin");
+		}
 	}
 }
